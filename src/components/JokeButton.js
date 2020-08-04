2680e353fec4320a15d7ceb8affd7341ccecedd2
Elise Bal <m11elbal@gmail.com>
2020-08-03 21:09:27 -0500

    uses Material UI button

--- src/components/JokeButton.js
+++ src/components/JokeButton.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
 
 export default class JokeButton extends React.Component {
   render() {
-    return <button type="button">Next Joke</button>;
+    return (
+      <Button variant="contained" color="primary">
+        Next Joke
+      </Button>
+    );
   }
 }
