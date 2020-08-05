477d4e0b595d3513cd93780b9a7c13e10648c50e
Elise Bal <m11elbal@gmail.com>
2020-08-04 19:25:02 -0500

    hits joke api and shows joke setup and punchline

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
 import React from 'react';
 import logo from './logo.svg';
 import JokeButton from './components/JokeButton';
+import axios from 'axios';
 
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <JokeButton />
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      setup: '',
+      punchline: '',
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    let jokeURL = 'https://official-joke-api.appspot.com/random_joke';
+    axios
+      .get(jokeURL)
+      .then((result) => {
+        console.log('result', result.data.setup);
+        this.setState({ setup: result.data.setup });
+        this.setState({ punchline: result.data.punchline });
+      })
+      .catch((error) => console.log(error));
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Need a laugh today? Click the button below!</h1>
+        <h2>{this.state.setup}</h2>
+        <h2>{this.state.punchline}</h2>
+        <JokeButton handleClick={this.handleClick} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/JokeButton.js
+++ src/components/JokeButton.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { Button } from '@material-ui/core';
+import axios from 'axios';
 
 export default class JokeButton extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
-      <div class="button-wrapper">
-        <Button variant="contained" color="primary">
+      <div className="button-wrapper">
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={this.props.handleClick}
+        >
           Next Joke
         </Button>
       </div>
